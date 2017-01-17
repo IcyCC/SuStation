574bf12c1fed0ce12d19f297261c74fe79c188d6
Su chang <1103232282@qq.com>
2017-01-17 12:15:50 +0800

    config

--- app/config.py
+++ app/config.py
@@ -1,2 +1,20 @@
 import os
-bas
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_COMMIT_TEARDOWN = True
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATEBASE_URL = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///'+os.path.join(basedir,'date-dev.sqlite')
+
+config = {
+    'development':DevelopmentConfig
+}
