701307a87c0793600bdd3608ec8f69e8279d5eaf
Su chang <1103232282@qq.com>
2017-01-18 15:32:48 +0800

    17.1.18start

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,14 @@
 from datetime import datetime
 from flask import render_template,session,redirect,url_for
 from .import main
-from .forms import*
-from ..import db
+from ..forms import*
+from .. import *
+from ..models import *
+
+@main.route('/',methods = ['GET','POST'])
+def index():
+    return render_template("index.html")
+
+@main.route('/Self',methods = ['GET','POST'])
+def Self_introduction():
+    return render_template("Self.html")
